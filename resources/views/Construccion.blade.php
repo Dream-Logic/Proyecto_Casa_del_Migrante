a315e1872a11431360cb7c310f7d6372b068b8d1
Tania <tania.calix@yahoo.com>
2020-10-12 13:17:19 -0600

    Update Construccion.blade.php
    Pagina redireccionadora

--- resources/views/Construccion.blade.php
+++ resources/views/Construccion.blade.php
@@ -1,20 +1,3 @@
-<html>
-<head>
-    <title>
-
-    </title>
-    <style>
-        body{
-            background-image: url("images/webconstruccion.jpg");
-           background-repeat: no-repeat;
-            background-size: 600px;
-            background-position: center;
-        }
-    </style>
-
-</head>
-<body>
-
-
-</body>
-</html>
+<div class="tres">
+    <img src="imagenes/web.jpg">
+</div>
