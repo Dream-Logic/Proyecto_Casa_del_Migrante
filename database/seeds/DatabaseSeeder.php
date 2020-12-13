18a8e1883b41a00fdc86cd03faa90858d47c351a
gaby15diaz <maridiaz4455@gmail.com>
2020-12-13 11:06:37 -0600

    elimine vistas y rutas que ya no eran utiles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         $this->call(UserTableSeeder::class);
-        $this->call(ProyectoSeeder::class);
+
     }
 }
