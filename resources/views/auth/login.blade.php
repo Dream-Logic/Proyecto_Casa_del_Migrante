09669a908a2ee581536eca09c5452275f97430d8
Daniela-Murillo1996 <murillodaniela408@gmail.com>
2020-12-13 20:27:21 -0600

    Arreglo en la vista de login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
 
 
 <div class="limiter">
-    <div class="container-login100" style="background-image: url('images/niña.jpg');">
+    <div class="container-login100" style="background-image: url('images/bienvenido/niña.jpg');">
         <div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
                     <strong style="color: #f8f9fb;font-size: 100%">Proyecto Casa Del Migrante </strong>
