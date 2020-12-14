8ad51785685ca04eec95891811fddea0492ba0b0
gaby15diaz <maridiaz4455@gmail.com>
2020-12-14 10:34:34 -0600

    elimine vistas y rutas que ya no eran utiles

--- app/Http/Controllers/paginaPrincipal_controller.php
+++ app/Http/Controllers/paginaPrincipal_controller.php
@@ -9,54 +9,7 @@ use Illuminate\Http\Request;
 
 class paginaPrincipal_controller extends Controller
 {
-    //
-    /*
 
-      //
-
-
-
-
-      public function create(){
-
-          return view('datosHuesped');
-      }
-
-      public function segundoFormulario()
-
-      {
-          //
-          return view('datosResponsable');
-
-      }
-
-      public function tercerFormulario()
-
-      {
-          //
-          return view('datosHechos');
-      }
-
-      public function cuartoFormulario()
-
-      {
-          //
-          return view('datosPersonal');
-
-      }
-
-      public function atras()
-
-      {
-          //
-          return view('index');
-      }
-      public function Btn_regresar()
-
-      {
-          //
-          return view('hola');
-      }*/
 
     public function principalformulario()
 
