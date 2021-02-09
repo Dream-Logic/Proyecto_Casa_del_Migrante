08f7cfb58c1223efb383a1ff4c9cf3b52583e6f1
Daniela-Murillo1996 <murillodaniela408@gmail.com>
2021-02-08 23:51:20 -0600

    Actualice listado huespedes en el area del director.

--- resources/views/SaludDirect.blade.php
+++ resources/views/SaludDirect.blade.php
@@ -14,11 +14,13 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
-    <div class="w3-container w3-teal mx-4" style="font-family: 'Raleway', sans-serif;">
-        <h6 class="mt-3" style="font-size: 30px; color: black;"><b>Listado de Enfermos</b></h6>
-    </div>
+    <a class="btn btn-success btn-warning" href="{{route('dire.access')}}">Regresar</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <a class="navbar-brand text-warning" href="#">LISTADO DE NIÑOS ENFERMOS</a>
     <form class="form-inline my-2 my-lg-0 ml-auto">
-        <div class="mx-4">
+
+        <div class="">
             <label class="card-title">Buscar por tipo</label>
         </div>
         <select name="tipo" class="form-control mr-sm-2" id="exampleFormControlSelect1">
@@ -28,27 +30,18 @@
             <option>identidad</option>
         </select>
         <input class="form-control mr-sm-2" name="buscarpor" type="search" aria-label="Search" placeholder="Buscar">
-        <button class="btn btn-success" type="submit">
-            <img src="/imagenes/iconos/buscar.svg" class="svg" width="25">
-        </button>
-        <a class="btn btn-warning" href="{{route('index.index')}}">
-            <img src="/imagenes/iconos/restaurar.svg" class="svg" width="30">
-        </a>
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
     </form>
-    <br>
-    <div class="table-responsive" style="-moz-box-shadow: 0px 5px 3px 3px rgba(194,194,194,1);
-    box-shadow: 0px 5px 3px 3px rgba(194,194,194,1);">
-        <table class="table ruler-vertical table-hover mx-sm-0 ">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">N°</th>
-                <th scope="col">Nombres</th>
-                <th scope="col">Apellidos</th>
-                <th scope="col">Identidad</th>
-                <th scope="col">Dirección</th>
-                <th scope="col">Enfermedad</th>
-                <th scope="col">Tratamiento</th>
-            </tr>
+    </nav>
+    <table class="table" >
+        <thead>
+        <tr class="w3-hover-green">
+            <th class="bg-dark text-white" style="font-family: 'Times New Roman'" scope="col">Id</th>
+            <th class="bg-dark text-white"style="font-family: 'Times New Roman'"  scope="col">Nombres</th>
+            <th class="bg-dark text-white" style="font-family: 'Times New Roman'"  scope="col">Apellidos</th>
+            <th class="bg-dark text-white"style="font-family: 'Times New Roman'"   scope="col">Enfermedad</th>
+            <th class="bg-dark text-white" style="font-family: 'Times New Roman'"  scope="col">Tratamiento</th>
+        </tr>
             </thead>
             <tbody>
             @forelse($listados as $huesped)
@@ -56,8 +49,6 @@
                 <th scope="row">{{ $huesped->id }}</th>
                 <td>{{ $huesped->nombres}} </td>
                 <td> {{ $huesped->apellidos }}</td>
-                <td> {{ $huesped->identidad }}</td>
-                <td> {{ $huesped->direccion }}</td>
                 <td>{{ $huesped->enfermedad }}</td>
                 <td>{{ $huesped->tratamiento}}</td>
       </tr>
